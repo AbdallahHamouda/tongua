262bdc6a3b40fc9e20360040437fd02f04ef4723
AbdallahHamouda <abdallahhamouda4@gmail.com>
2020-11-05 13:46:10 +0200

    bugs fixed

---
+++ precache-manifest.9d8434355dc42b2be09356cb18e95ad1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c258c98fa093e2beb7386e9da58d113",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "c93791b12610f2aab964",
+    "url": "/css/about.a4bbecac.css"
+  },
+  {
+    "revision": "12e44f59ab8ca9e80ea6",
+    "url": "/css/app.4833dcbe.css"
+  },
+  {
+    "revision": "520ec510274d2311a6ab7c6fcc27d214",
+    "url": "/img/Group 10.520ec510.png"
+  },
+  {
+    "revision": "6c906787b336dad7f1a3a203ec52b1fc",
+    "url": "/img/Group 22.6c906787.png"
+  },
+  {
+    "revision": "2b34bfbf906ce0ca7ba18f2cd8165167",
+    "url": "/img/Group 23.2b34bfbf.png"
+  },
+  {
+    "revision": "f0f86163fb75b8d78ccebb379ba6fd02",
+    "url": "/img/Group 8.f0f86163.png"
+  },
+  {
+    "revision": "36e9313136be7a952a94de31611604e8",
+    "url": "/img/email.36e93131.png"
+  },
+  {
+    "revision": "ad778fe009a608100cdc51f7a8638543",
+    "url": "/img/happy.ad778fe0.png"
+  },
+  {
+    "revision": "ab381c68aad811de4a903ee24981af6c",
+    "url": "/img/msg.ab381c68.png"
+  },
+  {
+    "revision": "0bbc42899b3515010e1ead3b61758816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93791b12610f2aab964",
+    "url": "/js/about.b82bf3cc.js"
+  },
+  {
+    "revision": "12e44f59ab8ca9e80ea6",
+    "url": "/js/app.0f1ccb00.js"
+  },
+  {
+    "revision": "b7166ea055e1069c1ea8",
+    "url": "/js/chunk-vendors.5df2793e.js"
+  },
+  {
+    "revision": "e6c1cd1859a977948179f71f840f6229",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.9d8434355dc42b2be09356cb18e95ad1.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "tongua-1"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
